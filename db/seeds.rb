616388c9d139fb06ab75dd2b3d706b3f1af1de02
royFloresNyc <royfloresnyc@gmail.com>
2020-08-17 17:26:50 -0400

    Trial run of populating Trivia table

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,10 +2,18 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
+
+#User.create(username: roy, )
+
 easy_general_knowledge = RestClient.get("https://opentdb.com/api.php?amount=50&category=9&difficulty=easy&type=multiple")
 parsed = JSON.parse(easy_general_knowledge)
-parsed
 
-Pry.start
+
+parsed["results"].each{ |question| Trivia.create(question)}
+
+
+
+
+#Pry.start
 
 
